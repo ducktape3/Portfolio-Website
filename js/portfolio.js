c0745eb516fc7fc1fd8ce85617bc340d497491a6
Harith Adam Hashim <151638913+ducktape3@users.noreply.github.com>
2025-01-13 02:00:03 +0800

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -74,6 +74,36 @@ document.querySelectorAll('header nav a').forEach(link => {
       if (scrollableGallery) {
           scrollableGallery.style.scrollBehavior = 'smooth';
       }
+
+      // Create skill indicators
+      document.querySelectorAll('.skill-box').forEach(skillBox => {
+          const skillIndicator = skillBox.querySelector('.skill-indicator');
+          const skillScale = parseInt(skillIndicator.getAttribute('data-scale'), 10);
+
+          for (let i = 1; i <= 10; i++) {
+              const rect = document.createElement('div');
+              rect.classList.add('indicator-box');
+              skillIndicator.appendChild(rect);
+          }
+
+          skillBox.addEventListener('mouseover', () => {
+              skillIndicator.querySelectorAll('.indicator-box').forEach((box, index) => {
+                  if (index < skillScale) {
+                      setTimeout(() => {
+                          box.classList.add('active');
+                      }, index * 100); // Delay activation
+                  }
+              });
+          });
+
+          skillBox.addEventListener('mouseout', () => {
+              skillIndicator.querySelectorAll('.indicator-box').forEach((box, index) => {
+                  setTimeout(() => {
+                      box.classList.remove('active');
+                  }, index * 100); // Delay deactivation
+              });
+          });
+      });
   });
   
   // Sticky Header
